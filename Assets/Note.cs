11378110e0e4ebd404229ddd0dab844346f2f6db
kinasch <janik@schoenberg.name>
2023-02-21 23:21:00 +0100

    3-musicgen-music:
    Added note percentages from data_acquisition
    Added generation based on percentages

--- Assets/Note.cs
+++ Assets/Note.cs
@@ -2,21 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrequencyAndDuration
+public class Note
 {
+    private string noteName;
     private double frequency;
     /// <summary>
     /// 0.25 for 1/4 Note
     /// </summary>
     private double duration;
 
-    public FrequencyAndDuration(double frequency, double duration)
+    public Note(string name, double frequency, double duration=0.5)
     {
+        this.noteName = name;
         this.frequency = frequency;
         this.duration = duration;
+    }
+
+    public Note(Note note)
+    {
+        this.noteName = note.Name;
+        this.frequency = note.Frequency;
+        this.duration = note.Duration;
     }
 
     public double Frequency => frequency;
 
     public double Duration => duration;
+    
+    public string Name => noteName;
 }
--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using UnityEngine;
 using Object = System.Object;
 using Random = UnityEngine.Random;
@@ -9,77 +11,109 @@ public class Oscillator : MonoBehaviour
 {
     private double increment;
     private double phase;
-    private int cord = 0;
-    private double samplingFrequency = 48000.0;
+    private const double samplingFrequency = 48000.0;
+
+    [SerializeField] private int musicSeed = 420;
+    private int seed;
+    private int[] innerSeeds;
 
     [SerializeField] private float gain;
     private float currentGain;
-    [SerializeField] private float bpm=100f;
+    [Range(75,125)] public float bpm=100f;
+    [SerializeField] private int amountOfInnerSeeds = 4;
+    
     private double time=0.0;
-    [SerializeField] private List<FrequencyAndDuration> frequencies;
     private double duration = 0;
+    private Note oldNote, currNote;
+    private float randomValue;
+    private bool changeNote = false;
+
+    private readonly Dictionary<string, double[]> chances = new Dictionary<string, double[]>()
+    {
+        {"C#",new double[]{0.055,0.0519,0.0744,0.0706,0.2895,0.0641,0.0631,0.0765,0.0374,0.1063,0.0165,0.0947}},
+        { "G#",new double[]{0.0441, 0.07, 0.0729, 0.0904, 0.0602, 0.0384, 0.0677, 0.0503, 0.0397, 0.1072, 0.0436, 0.3156} },
+        { "D#",new double[]{0.0237, 0.0763, 0.0478, 0.0945, 0.0531, 0.0752, 0.2642, 0.0599, 0.0757, 0.0756, 0.0626, 0.0915} },
+        { "B",new double[]{0.0756, 0.0467, 0.2355, 0.1226, 0.042, 0.0877, 0.0258, 0.0942, 0.0169, 0.1534, 0.0477, 0.052} },
+        { "F#",new double[]{0.0392, 0.0545, 0.1027, 0.0814, 0.0398, 0.0672, 0.0302, 0.0647, 0.0263, 0.3924, 0.0345, 0.0671} },
+        { "C",new double[]{0.0478, 0.0591, 0.0923, 0.2844, 0.0279, 0.1, 0.0473, 0.0667, 0.0701, 0.0844, 0.0708, 0.0493} },
+        { "G",new double[]{0.0916, 0.0574, 0.0486, 0.1107, 0.01, 0.1086, 0.0363, 0.0726, 0.0729, 0.052, 0.3032, 0.0359} },
+        { "D",new double[]{0.0736, 0.0471, 0.062, 0.1084, 0.0313, 0.3049, 0.0382, 0.0897, 0.0564, 0.073, 0.0884, 0.0272} },
+        { "A",new double[]{0.3213, 0.0469, 0.0797, 0.082, 0.0292, 0.1015, 0.0161, 0.0976, 0.0463, 0.0571, 0.0855, 0.0368} },
+        { "E",new double[]{0.0947, 0.0286, 0.0877, 0.0847, 0.0355, 0.0929, 0.0334, 0.2788, 0.0528, 0.0984, 0.0733, 0.0392} },
+        { "F",new double[]{0.0582, 0.0776, 0.0271, 0.1204, 0.0259, 0.0775, 0.0587, 0.073, 0.2864, 0.0471, 0.0959, 0.0522} },
+        { "A#",new double[]{0.0585, 0.2688, 0.0621, 0.1114, 0.0407, 0.0718, 0.0614, 0.0323, 0.077, 0.0891, 0.0603, 0.0666} }
+    };
+
+    private readonly List<Note> notes = new List<Note>()
+    {
+        new("C#", 277.18),
+        new( "G#", 392.00),
+        new( "D#", 311.13),
+        new( "B", 493.88),
+        new( "F#", 369.99),
+        new( "C", 261.63),
+        new( "G", 415.30),
+        new( "D", 293.66),
+        new( "A", 440.00),
+        new( "E", 329.63),
+        new( "F", 349.23),
+        new( "A#", 466.16)
+    };
+
+    private int repetitionCounter = 0;
+    private int currentSeedNumber = 0;
 
     private void Start()
     {
-        frequencies = new List<FrequencyAndDuration>()
-        {
-            new(200.0,0.125),
-            new(250.0,0.125),
-            new(300.0,0.125),
-            new(350.0,0.125),
-            new(400.0,0.25),
-            new(400.0,0.25),
-            
-            new(450.0,0.125),
-            new(450.0,0.125),
-            new(450.0,0.125),
-            new(450.0,0.125),
-            new(400.0,0.25),
-            new(0.0,0.25),
-            
-            new(450.0,0.125),
-            new(450.0,0.125),
-            new(450.0,0.125),
-            new(450.0,0.125),
-            new(400.0,0.25),
-            new(0.0,0.25),
-            
-            new(350.0,0.125),
-            new(350.0,0.125),
-            new(350.0,0.125),
-            new(350.0,0.125),
-            new(300.0,0.25),
-            new(300.0,0.25),
-            
-            new(400.0,0.125),
-            new(400.0,0.125),
-            new(400.0,0.125),
-            new(400.0,0.125),
-            new(200.0,0.5),
-            
-            
-        };
+        
+        Random.InitState(musicSeed);
+        innerSeeds = CreateSeedList(musicSeed, 10000);
 
-        duration = frequencies[0].Duration * 4 * (60/bpm) * 2; // 2|4 für alle meine Entchen, deswegen * 2 am Ende, bei 4/4 nichts
+        seed = innerSeeds[currentSeedNumber];
+        
+        Random.InitState(seed);
+
+        duration = Random.Range(0.125f,0.25f) * 4 * (60 / bpm);
         currentGain = gain;
+        
+        currNote = notes[Random.Range(0,notes.Count)];
+        oldNote = new Note(currNote);
+
+        randomValue = Random.value;
     }
 
     private void Update()
     {
+        // Stops working once Time.timeScale = 0 -> Fix?
         time += Time.deltaTime;
         currentGain = Mathf.Lerp(gain, 0, (float)time);
         if (time >= duration)
         {
+            changeNote = true;
             currentGain = gain;
-            cord = (cord + 1) % frequencies.Count;
             time = 0.0;
-            duration = frequencies[cord].Duration * 4 * (60 / bpm);
+            duration = 0.125 * 4 * (60 / bpm);
+            repetitionCounter++;
+        }
+        
+        if (repetitionCounter > 8)
+        {
+            repetitionCounter %= 8;
+            ChangeInnerSeed();
         }
     }
 
     private void OnAudioFilterRead(float[] data, int channels)
     {
-        increment = (frequencies[cord].Frequency * 2.0 * Mathf.PI / samplingFrequency);
+        if (changeNote)
+        {
+            currNote = GetNewNote(oldNote);
+            oldNote = new Note(currNote);
+            Debug.Log("Current Note: "+currNote.Name+"\n"+seed);
+            changeNote = false;
+        }
+
+        increment = ( currNote.Frequency * 2.0 * Mathf.PI / samplingFrequency);
         
         for (int i = 0; i < data.Length; i += channels)
         {
@@ -96,5 +130,100 @@ public class Oscillator : MonoBehaviour
 
             phase %= (Mathf.PI * 2);
         }
+    }
+
+    private Note GetNewNote(Note prevNote)
+    {
+        double[] notePercentages = SumEveryDoubleArrayValueWithItsPreviousValues(chances[prevNote.Name]);
+        // Sorted alphabetically (for whatever reason)
+        string[] correspondingNote = {"A", "A#","B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
+        float tempRdmValue = repetitionCounter % 2 == 0 ? randomValue : 1 - randomValue;
+        for (var i=notePercentages.Length-1; i>=0;i--)
+        {
+            if (tempRdmValue > notePercentages[i])
+            {
+                var newName = correspondingNote[i];
+
+                // Prevent same note repetition.
+                // Replace with something more elegant later on.
+                if (newName == prevNote.Name)
+                {
+                    var num = Array.IndexOf(correspondingNote, prevNote.Name) + (int)(Math.Floor(bpm/50));
+                    var altNote = FindInNotes(correspondingNote[num%correspondingNote.Length]);
+                    return altNote;
+                }
+                
+                var returnNote = FindInNotes(newName);
+                return returnNote;
+            }
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// That's resource hungry.
+    /// Change!
+    /// </summary>
+    private double[] SumEveryDoubleArrayValueWithItsPreviousValues(double[] array)
+    {
+        var newArray = new double[array.Length];
+        //array.CopyTo(newArray,0);
+        newArray[0] = 0;
+        for (int i = 1; i < array.Length; i++)
+        {
+            newArray[i] += (newArray[i - 1] + array[i-1]);
+        }
+        
+        return newArray;
+    }
+
+    private Note FindInNotes(string nameToLookup)
+    {
+        foreach (var note in notes)
+        {
+            if (note.Name == nameToLookup) return note;
+        }
+
+        return null;
+    }
+    
+    public void SetSeed(string inputSeed)
+    {
+        this.musicSeed = int.Parse(inputSeed);
+        
+        Random.InitState(musicSeed);
+        
+        innerSeeds = CreateSeedList(musicSeed, 10000);
+
+        seed = innerSeeds[0];
+        
+        Random.InitState(seed);
+        
+        this.randomValue = Random.value;
+    }
+    
+    private void ChangeInnerSeed()
+    {
+        currentSeedNumber = (currentSeedNumber + 1) % innerSeeds.Length;
+
+        this.seed = innerSeeds[currentSeedNumber];
+        
+        Random.InitState(seed);
+        randomValue = Random.value;
+    }
+
+    private int[] CreateSeedList(int initialSeed, int max)
+    {
+        Random.InitState(initialSeed);
+        
+        int[] newSeeds = new int[amountOfInnerSeeds];
+        for (int i = 0; i < amountOfInnerSeeds; i++)
+        {
+            // Usually first multiplying and then dividing is better.
+            int rangeMax = max / amountOfInnerSeeds;
+            newSeeds[i] = Random.Range(i * rangeMax, (i + 1) * rangeMax);
+        }
+
+        return newSeeds;
     }
 }
